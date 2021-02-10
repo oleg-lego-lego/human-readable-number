5d210990018ed7c75884a1d8a58b72a39308b9ab
Oleg Ostrovski <oleg.ostrovski@gmail.com>
2021-02-10 22:00:10 +0300

    task human-readable-number is comleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let hundreds = Math.floor(number / 100);
+    let tens = Math.floor((number - hundreds * 100) / 10);
+    let ones = number - hundreds * 100 - tens * 10;
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    if (tens === 1) {
+        tens = 0;
+        ones = number - hundreds * 100;
+    }
+
+    const onesArray = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tensArray = [
+        "",
+        "",
+        "twenty ",
+        "thirty ",
+        "forty ",
+        "fifty ",
+        "sixty ",
+        "seventy ",
+        "eighty ",
+        "ninety ",
+    ];
+
+    let hundredsArray = hundreds ? onesArray[hundreds] + " hundred " : "";
+
+    return (hundredsArray + tensArray[tens] + onesArray[ones]).trim();
+};
